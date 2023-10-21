023f5d8b096f43cfb8264ab775eeb7d984fbd0f3
Ananya Jajodia <aj477@cornell.edu>
2023-10-21 10:02:04 -0400

    Modify board.ml and board.mli

--- sequence/lib/board.ml
+++ sequence/lib/board.ml
@@ -1,27 +1,29 @@
-(* Defines type for board *)
 open Deck
 
-type card = Reg_Card of Deck.t | Free_space
-type chip = Red | Blue | Free | None
-type square = { row : int; col : int; mutable chip : chip; card : card }
-type t = square list list
+(* Defines type for board *)
+module Board = struct
+  type card = Reg_Card of Deck.t | Free_space
+  type chip = Red | Blue | Free | None
+  type square = { row : int; col : int; mutable chip : chip; card : card }
+  type t = square list list
 
-(* returns new board with no chips placed *)
-let init : t = failwith "Unimplimented"
+  (* returns new board with no chips placed *)
+  let init : t = failwith "Unimplimented"
 
-(* modifies board [b] and places chip [ch] in the spot (r, c) *)
-let place_chip (ch : chip) (r : int) (c : int) (b : t) : unit =
-  failwith "Unimplimented"
+  (* modifies board [b] and places chip [ch] in the spot (r, c) *)
+  let place_chip (ch : chip) (r : int) (c : int) (b : t) : unit =
+    failwith "Unimplimented"
 
-(* modifies board [b] and removes any chip in the spot (r, c) *)
-let remove_chip (r : int) (c : int) (b : t) : unit = failwith "Unimplimented"
+  (* modifies board [b] and removes any chip in the spot (r, c) *)
+  let remove_chip (r : int) (c : int) (b : t) : unit = failwith "Unimplimented"
 
-(* returns the chip in space (r,c). Returns None if its empty,
-   Free if its the free space, or the color of the player who hold the space*)
-let check_space (r : int) (c : int) (b : t) : chip = failwith "Unimplimented"
+  (* returns the chip in space (r,c). Returns None if its empty,
+     Free if its the free space, or the color of the player who hold the space*)
+  let check_space (r : int) (c : int) (b : t) : chip = failwith "Unimplimented"
 
-(* returns true if there is a win on the board*)
-let is_win (b : t) : bool = failwith "Unimplimented"
+  (* returns true if there is a win on the board*)
+  let is_win (b : t) : bool = failwith "Unimplimented"
 
-(* prints ascii version of the board *)
-let print_board (b : t) : unit = failwith "Unimplimented"
+  (* prints ascii version of the board *)
+  let print_board (b : t) : unit = failwith "Unimplimented"
+end
--- sequence/lib/board.mli
+++ sequence/lib/board.mli
@@ -1,26 +1,30 @@
-(* Defines type for board *)
 open Deck
 
-type card = Reg_Card of Deck.t | Free_space
-type chip = Red | Blue | Free | None
-type square = { row : int; col : int; mutable chip : chip; card : card }
-type t = square list list
+(* Defines type for board *)
+module type Board = sig
+  type card = Reg_Card of Deck.t | Free_space
+  type chip = Red | Blue | Free | None
+  type square = { row : int; col : int; mutable chip : chip; card : card }
 
-(* returns new board with no chips placed *)
-val init : t
+  (* Representation type*)
+  type t = square list list
 
-(* modifies board [b] and places chip [ch] in the spot (r, c) *)
-val place_chip : chip -> int -> int -> t -> unit
+  (* returns new board with no chips placed *)
+  val init : t
 
-(* modifies board [b] and removes any chip in the spot (r, c) *)
-val remove_chip : int -> int -> t -> unit
+  (* modifies board [b] and places chip [ch] in the spot (r, c) *)
+  val place_chip : chip -> int -> int -> t -> unit
 
-(* returns the chip in space (r,c). Returns None if its empty,
-   Free if its the free space, or the color of the player who hold the space*)
-val check_space : int -> int -> t -> chip
+  (* modifies board [b] and removes any chip in the spot (r, c) *)
+  val remove_chip : int -> int -> t -> unit
 
-(* returns true if there is a win on the board*)
-val is_win : t -> bool
+  (* returns the chip in space (r,c). Returns None if its empty,
+     Free if its the free space, or the color of the player who hold the space*)
+  val check_space : int -> int -> t -> chip
 
-(* prints ascii version of the board *)
-val print_board : t -> unit
+  (* returns true if there is a win on the board*)
+  val is_win : t -> bool
+
+  (* prints ascii version of the board *)
+  val print_board : t -> unit
+end
