39442d46f0aa6dcb78ba778f92a8654b79995764
Ananya Jajodia <aj477@cornell.edu>
2023-10-21 09:45:44 -0400

    Fix Board.mli
    - fixed the syntax of Board.mli

--- sequence/lib/board.mli
+++ sequence/lib/board.mli
@@ -1,26 +1,26 @@
 (* Defines type for board *)
 open Deck
-type card : Deck.t | Free_space
-type chip : Red | Blue | Free | None
-type square : {row: int; col :int; mutable chip: Chip; card: Card}
-type t : square list list
+
+type card = Reg_Card of Deck.t | Free_space
+type chip = Red | Blue | Free | None
+type square = { row : int; col : int; mutable chip : chip; card : card }
+type t = square list list
 
 (* returns new board with no chips placed *)
 val init : t
 
 (* modifies board [b] and places chip [ch] in the spot (r, c) *)
-val place_chip (ch : chip) (r : int) (c:int) (b: t) : unit
+val place_chip : chip -> int -> int -> t -> unit
 
 (* modifies board [b] and removes any chip in the spot (r, c) *)
-val remove_chip (r : int) (c:int) (b: t) : unit
+val remove_chip : int -> int -> t -> unit
 
-(* returns the chip in space (r,c). Returns None if its empty, 
+(* returns the chip in space (r,c). Returns None if its empty,
    Free if its the free space, or the color of the player who hold the space*)
-val check_space (r: int) (c: int) (b: t) : chip 
+val check_space : int -> int -> t -> chip
 
 (* returns true if there is a win on the board*)
-val is_win (b: t) : bool
+val is_win : t -> bool
 
 (* prints ascii version of the board *)
-val print_board (b:t) : unit
-
+val print_board : t -> unit
