68e343181b953ab29a75840e9e5a0eb6277a0fd2
Ananya Jajodia <aj477@cornell.edu>
2023-10-21 10:41:31 -0400

    Modify board.ml and board.mli
    - added id field to t
    - began implementation of init
    - implemented place_chip

--- sequence/lib/board.ml
+++ sequence/lib/board.ml
@@ -4,15 +4,85 @@ open Deck
 module Board = struct
   type card = Reg_Card of Deck.t | Free_space
   type chip = Red | Blue | Free | None
-  type square = { row : int; col : int; mutable chip : chip; card : card }
+
+  type square = {
+    row : int;
+    col : int;
+    mutable chip : chip;
+    card : card;
+    id : int option;
+  }
+
   type t = square list list
 
   (* returns new board with no chips placed *)
-  let init : t = failwith "Unimplimented"
+  let init : t =
+    [
+      [
+        { row = 0; col = 0; chip = Free; card = Free_space; id = None };
+        {
+          row = 0;
+          col = 1;
+          chip = None;
+          card = Reg_Card { suit = Spades; rank = Ten };
+          id = Some 1;
+        };
+        {
+          row = 0;
+          col = 2;
+          chip = None;
+          card = Reg_Card { suit = Spades; rank = Queen };
+          id = Some 1;
+        };
+        {
+          row = 0;
+          col = 3;
+          chip = None;
+          card = Reg_Card { suit = Spades; rank = King };
+          id = Some 1;
+        };
+        {
+          row = 0;
+          col = 4;
+          chip = None;
+          card = Reg_Card { suit = Spades; rank = Ace };
+          id = Some 1;
+        };
+        {
+          row = 0;
+          col = 5;
+          chip = None;
+          card = Reg_Card { suit = Diamonds; rank = Two };
+          id = Some 0;
+        };
+        {
+          row = 0;
+          col = 6;
+          chip = None;
+          card = Reg_Card { suit = Diamonds; rank = Three };
+          id = Some 0;
+        };
+        {
+          row = 0;
+          col = 7;
+          chip = None;
+          card = Reg_Card { suit = Diamonds; rank = Four };
+          id = Some 0;
+        };
+        {
+          row = 0;
+          col = 8;
+          chip = None;
+          card = Reg_Card { suit = Diamonds; rank = Five };
+          id = Some 0;
+        };
+        { row = 0; col = 9; chip = Free; card = Free_space; id = None };
+      ];
+    ]
 
   (* modifies board [b] and places chip [ch] in the spot (r, c) *)
   let place_chip (ch : chip) (r : int) (c : int) (b : t) : unit =
-    failwith "Unimplimented"
+    (List.nth (List.nth b r) c).chip <- ch
 
   (* modifies board [b] and removes any chip in the spot (r, c) *)
   let remove_chip (r : int) (c : int) (b : t) : unit = failwith "Unimplimented"
