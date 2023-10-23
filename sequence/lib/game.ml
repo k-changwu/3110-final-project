15d25d8665e08944962daf497bac37b99f25737a
ch685 <ch685@cornell.edu>
2023-10-22 21:38:33 -0400

    Create game.ml and game.mli files - Define a type for a game - Consists of a record for current_player_id, the current players, the current deck, the board, and the current_card being played - Define a type result (whether game is still in session or ended in a win/draw) - Write function definition to start the game - This includes shuffling a deck and then create a Game.t that fills in the fields - Write function definition of current_player, to get the current player - Write function definition of next_player, to get the next player - Write function definition for draw_card, to draw a card from the deck.

---
+++ sequence/lib/game.ml
@@ -0,0 +1,36 @@
+(* game.ml *)
+type t = {
+  mutable current_player_id : int;
+  players : Player.t array; (* array of players *)
+  mutable deck : Deck.t list; (* remaining deck *)
+  board : Board.t; (* game board *)
+  mutable current_card : Deck.t; (* current card to play *)
+}
+
+type result = Ongoing | Won of int | Draw
+
+let start () =
+  let deck_shuffled = Deck.full_deck () |> Deck.shuffle in
+  {
+    current_player_id = 1;
+    players = [| Player.create 1; Player.create 2 |];
+    (* initialize two players *)
+    deck = deck_shuffled;
+    board = Board.init;
+    (* initialize a new game board *)
+    current_card = List.hd deck_shuffled;
+    (* current card to play *)
+  }
+
+let current_player game = game.current_player_id
+
+let next_player game =
+  game.current_player_id <- (if game.current_player_id = 1 then 2 else 1)
+
+let draw_card game =
+  match game.deck with
+  | [] -> None (* deck is empty *)
+  | h :: t ->
+      game.deck <- t;
+      game.current_card <- h;
+      Some h
---
+++ sequence/lib/game.mli
@@ -0,0 +1,20 @@
+(* game.mli *)
+type t
+
+(* Represents the result of a game, whether it's ongoing, won, or a draw. *)
+type result = Ongoing | Won of int | Draw
+
+(* Starts a new game with two players. *)
+val start : unit -> t
+
+(* Gets the current player's ID. *)
+val current_player : t -> int
+
+(* Handles a player's turn with the given card and board position. Returns an updated game state. *)
+val play_turn : t -> int -> int -> t
+
+(* Checks the game state for a win or draw. *)
+val check_game_over : t -> result
+
+(* Returns the card that the current player can play. *)
+val current_card : t -> Deck.t
