0f71923517356105d58b96d1eaf0c039ca52a79d
Ananya Jajodia <aj477@cornell.edu>
2023-11-30 16:53:03 -0500

    Implemented make game to run game in terminal
    - took out sequence from bin dune file so it would compile (needs to be added back)
    - edited main to print a thing
    - added 'make chat' command to make file

--- sequence/bin/main.ml
+++ sequence/bin/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline "Hello, World!"
+(* open Sequence open Board *)
+
+let () =
+  print_endline "\nWelcome to Sequence!\n";
+  print_endline
+    "Sequence is a two player game of connect 5. Looks up the rules online to \
+     find out more\n"
