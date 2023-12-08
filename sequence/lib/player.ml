370e5d9cabf402c7265126a244e54b566b786cf0
Katherine Chang Wu <kc842@cornell.edu>
2023-12-08 17:01:55 -0500

    pushing test cases again

--- sequence/lib/player.ml
+++ sequence/lib/player.ml
@@ -26,16 +26,15 @@ let get_hand player = player.hand
 (* play_card function simulates a player playing a card from their hand. It
    takes a player and the card to be played and returns a new player with the
    same ID and a hand that no longer contains the played card. *)
+let play_card player card =
+  if has_card player card then
+    Some { player with hand = List.filter (fun c -> c <> card) player.hand }
+  else None
 
 (* has_card function checks if the card a player wants to play exists in their
    deck *)
 let has_card player target_card =
   List.exists (fun card -> card = target_card) player.hand
-
-let play_card player card =
-  if has_card player card then
-    Some { player with hand = List.filter (fun c -> c <> card) player.hand }
-  else None
 
 (* get_id function returns the player's id *)
 let get_id player = player.id
