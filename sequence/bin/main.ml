36bd7806341aaecbf86e5612c97b0f692620e571
Ananya Jajodia <aj477@cornell.edu>
2023-12-08 18:05:18 -0500

    copied board.txt and worked on make game main file

--- sequence/bin/main.ml
+++ sequence/bin/main.ml
@@ -1,7 +1,15 @@
-(* open Sequence open Board *)
+open Sequence.Game
+open Sequence.Player
+open Sequence.Board
+open Sequence.Deck
 
 let () =
   print_endline "\nWelcome to Sequence!\n";
   print_endline
     "Sequence is a two player game of connect 5. Looks up the rules online to \
-     find out more\n"
+     find out more\n";
+  let g = Sequence.Game.start () in
+  print_endline
+    ("\n It's player "
+    ^ string_of_int (Sequence.Game.current_player g)
+    ^ "'s turn!")
--- sequence/lib/game.ml
+++ sequence/lib/game.ml
@@ -4,9 +4,9 @@ type t = {
   players : Player.t array; (* array of players *)
   mutable deck : Deck.t list; (* remaining deck *)
   board : Board.t;
-      (* game board *)
-      (* current card to play *)
-      (* mutable current_card : Deck.t; *)
+  (* game board *)
+  (* current card to play *)
+  mutable current_card : Deck.t;
 }
 
 type result =
@@ -23,7 +23,7 @@ let start () =
     deck = deck_shuffled;
     board = Board.init;
     (* initialize a new game board *)
-    (* current_card = List.hd deck_shuffled; *)
+    current_card = List.hd deck_shuffled;
     (* current card to play *)
   }
 
